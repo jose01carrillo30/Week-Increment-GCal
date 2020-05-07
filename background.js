9e188ddfb8b8fe251298ce334297997a5499075b
jose01carrillo30 <34354911+PraiseTheGoldenHoodie@users.noreply.github.com>
2020-05-06 19:18:02 -0500

    init commit

---
+++ background.js
@@ -0,0 +1,70 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#000000'}, function() {
+    console.log('Pop-up button clicked.');
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'calendar.google.com'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  if (command == "down-week" || command == "up-week") {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var bkg = chrome.extension.getBackgroundPage();
+
+      var tabUrl = tabs[0].url;
+      if (!tabUrl.includes("customweek")) {
+        bkg.console.log("Please select custom week!");
+        return;
+      }
+  
+      // ----------- get the currently selected date 'currDate' ----------- //
+      var urlPieces = tabUrl.split("/");
+      var urlDate = [];
+      urlDate.isValid = false;
+      var currDate;
+      if (urlPieces.length > 3) {
+        urlDate.isValid = true;
+        for (var i = 0; i < 3; i++) {
+          bkg.console.log("urlpice: " + urlPieces[urlPieces.length - 1 - i]);
+          urlDate[i] = parseInt(urlPieces[urlPieces.length - 1 - i], 10);
+          if (isNaN(urlDate[i])) {
+            urlDate.isValid = false;
+            break;
+          }
+        }
+      }
+      if (!urlDate.isValid) {
+        bkg.console.log("dateless url found");
+        currDate = new Date();
+      } else {
+        currDate = new Date(urlDate[2], urlDate[1]-1, urlDate[0]);
+      }
+  
+      var tabTitle = encodeURIComponent(tabs[0].title);
+  
+      // ----------- Update URL ----------- //
+      // increment date one week
+      if (command == "down-week") {
+        currDate.setDate(currDate.getDate() + 7);
+      } else {
+        currDate.setDate(currDate.getDate() - 7);
+      }
+  
+      // set target URL with updated date
+      tabUrl = "https://calendar.google.com/calendar/r/customweek/" //FIXME: get this dynamically
+          + currDate.getFullYear() + "/" + (currDate.getMonth() + 1) + "/" + currDate.getDate();
+  
+      bkg.console.log('taburl: ' + tabUrl + "   tab title: " + tabTitle);
+  
+      chrome.tabs.update({url: tabUrl});
+    });
+  }
+});
---
+++ popup.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var bkg = chrome.extension.getBackgroundPage();
+    bkg.console.log('foo');
+  });
+};
